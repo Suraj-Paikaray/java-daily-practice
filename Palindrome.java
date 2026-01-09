07caf872e131e9165694b969ef48c44d63d4987e
Suraj Paikaray <pratapsuraj2226@gmail.com>
2026-01-10 02:19:54 +0530

    09/01/2026 java practice

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ package javaPrograms;
 import java.util.Scanner;
 
 public class Palindrome {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
@@ -30,3 +30,5 @@ public class Palindrome {
 }
 
 }
+    
+
